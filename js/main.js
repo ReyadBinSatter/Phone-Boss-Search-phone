b95126d7f149109ffdda463a91ba94d36579edd6
ReyadBinSatter <rbsreyad@gmail.com>
2022-03-02 19:56:48 +0600

    some card edit

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const searchPhones = () => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchPhoneName}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => phoneResults(data.data))
+        .then(data => phoneResults(data.data.slice(0,10)))
 
 }
 
@@ -16,7 +16,6 @@ const phoneResults = (phones) => {
     const phoneSearchResult = document.getElementById('phone-result');
     phoneSearchResult.textContent = '';
     phones.forEach(phone => {
-        console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
@@ -29,8 +28,10 @@ const phoneResults = (phones) => {
                     <button onclick="phoneDetail('${phone.slug}')" class="btn btn-primary" type="button"
                     id="button-card">Details</button>
                 </div>
-        `
-        phoneSearchResult.appendChild(div);
+  `
+            phoneSearchResult.appendChild(div);
+   
+        
     });
 }
 
@@ -49,25 +50,49 @@ const phoneDetail = phoneId =>{
 const singlePhoneDetails = details =>{
     console.log(details);
     const phoneDetails = document.getElementById('phone-details');
+    phoneDetails.textContent = '';
     const div = document.createElement('div');
     div.classList.add('card');
-    div.innerHTML = `
+    if(details.releaseDate==''){
+        div.innerHTML = `
+    <img src="${details.image}" class="card-img-top w-50 mx-auto" alt="...">
+    <div class="card-body mx-auto">
+        <h3 class="card-title">${details.name}</h3>
+        <h5 class="card-title-1">${details.brand}</h5>
+        <p class="card-text"><strong>Chip set :</strong> ${details.mainFeatures.chipSet}</p>
+        <p class="card-text"><strong>Display Size :</strong> ${details.mainFeatures.displaySize}</p>
+        <p class="card-text"><strong>Memory :</strong> ${details.mainFeatures.memory}</p>
+        <h5 class="card-title-1">Others</h5>
+        <p class="card-text"><strong>Bluetooth :</strong> ${details.others.Bluetooth}</p>
+        <p class="card-text"><strong>GPS :</strong> ${details.others.GPS}</p>
+        <p class="card-text"><strong>NFC :</strong> ${details.others.NFC}</p>
+        <p class="card-text"><strong>Radio :</strong> ${details.others.Radio}</p>
+        <p class="card-text"><strong>USB :</strong> ${details.others.USB}</p>
+        <p class="card-text"><strong>WLAN :</strong> ${details.others.WLAN}</p>
+        <p class="card-text"><strong>Sensor :</strong> ${details.mainFeatures.sensors}</p>
+    </div>
+    `;
+    }
+    else{
+        div.innerHTML = `
     <img src="${details.image}" class="card-img-top w-50" alt="...">
     <div class="card-body">
         <h3 class="card-title">${details.name}</h3>
         <h5 class="card-title-1">${details.brand}</h5>
-        <p class="card-text">Release date : ${details.releaseDate}</p>
-        <p class="card-text">Chip set : ${details.mainFeatures.chipSet}</p>
-        <p class="card-text">Display Size : ${details.mainFeatures.displaySize}</p>
-        <p class="card-text">Memory : ${details.mainFeatures.memory}</p>
+        <p class="card-text"><strong>Release date :</strong> ${details.releaseDate}</p>
+        <p class="card-text"><strong>Chip set :</strong> ${details.mainFeatures.chipSet}</p>
+        <p class="card-text"><strong>Display Size :</strong> ${details.mainFeatures.displaySize}</p>
+        <p class="card-text"><strong>Memory :</strong> ${details.mainFeatures.memory}</p>
         <h5 class="card-title-1">Others</h5>
-        <p class="card-text">Bluetooth : ${details.other.Bluetooth}</p>
-        <p class="card-text">GPS : ${details.other.GPS}</p>
-        <p class="card-text">NFC : ${details.other.NFC}</p>
-        <p class="card-text">Radio : ${details.other.Radio}</p>
-        <p class="card-text">USB : ${details.other.USB}</p>
-        <p class="card-text">WLAN : ${details.other.WLAN}</p>
+        <p class="card-text"><strong>Bluetooth :</strong> ${details.others.Bluetooth}</p>
+        <p class="card-text"><strong>GPS :</strong> ${details.others.GPS}</p>
+        <p class="card-text"><strong>NFC :</strong> ${details.others.NFC}</p>
+        <p class="card-text"><strong>Radio :</strong> ${details.others.Radio}</p>
+        <p class="card-text"><strong>USB :</strong> ${details.others.USB}</p>
+        <p class="card-text"><strong>WLAN :</strong> ${details.others.WLAN}</p>
+        <p class="card-text"><strong>Sensor :</strong> ${details.mainFeatures.sensors}</p>
     </div>
     `;
+    }
     phoneDetails.appendChild(div);
 }
