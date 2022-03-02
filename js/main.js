c0c84525f8d45c457420b8d5a58b072fb2ef2071
ReyadBinSatter <rbsreyad@gmail.com>
2022-03-02 17:16:19 +0600

    card and start details

--- js/main.js
+++ js/main.js
@@ -9,13 +9,31 @@ const searchPhones = () => {
     fetch(url)
         .then(res => res.json())
         .then(data => phoneResults(data.data))
-       
+
 }
 
-const phoneResults = (phones) =>{
+const phoneResults = (phones) => {
     const phoneSearchResult = document.getElementById('phone-result');
+    phoneSearchResult.textContent = '';
     phones.forEach(phone => {
-        //console.log(phone);
-
+        console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+                <div onclick="phoneDetail('${phone.slug}')" class="card">
+                    <img src="${phone.image}" class="card-img-top w-50" alt="...">
+                    <div class="card-body">
+                    <h3 class="card-title">${phone.phone_name}</h3>
+                    <h5 class="card-title-1">${phone.brand}</h5>
+                    </div>
+                </div>
+        `
+        phoneSearchResult.appendChild(div);
     });
+}
+
+
+
+const phoneDetail = phoneId =>{
+    console.log(phoneId);
 }
